b21372b98fbcc4b54715fd2a3d03b15edd3db7a3
hanifbinz <hanifbinz@gmail.com>
2026-08-03 13:30:05 +0700

    Update route dan view admin

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
+            <h2 class="font-semibold text-xl text-gray-800 border-b pb-4 mb-4">Kelola Pengguna (Users)</h2>
+            <p class="text-gray-600">Halaman ini siap digunakan untuk membuat fitur Kelola Admin/Users nantinya.</p>
+        </div>
+    </div>
+</div>
+@endsection
